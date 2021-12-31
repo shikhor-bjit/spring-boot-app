33fe794b0f6273219b2979d59e12350bf1fc0f12
shikhor.kumer <shikhor.kumer@bjitgroup.com>
2021-12-31 12:14:43 +0600

    Initiate Spring Boot Application

---
+++ src/main/java/com/example/springbootapp/SpringBootAppApplication.java
@@ -0,0 +1,21 @@
+package com.example.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Welcome to Spring Boot Application!";
+    }
+
+}
