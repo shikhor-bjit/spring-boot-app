e6360fb191eddfc63887d1ef824243380b1e9054
shikhor.kumer <shikhor.kumer@bjitgroup.com>
2021-12-31 13:26:17 +0600

    New API added

--- src/main/java/com/example/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/example/springbootapp/SpringBootAppApplication.java
@@ -3,6 +3,7 @@ package com.example.springbootapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,6 +17,11 @@ public class SpringBootAppApplication {
     @GetMapping("/hello")
     public String sayHello() {
         return "Welcome to Spring Boot Application!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHelloToUser(@PathVariable String name) {
+        return "Welcome to Spring Boot Application! Hello " + name;
     }
 
 }
